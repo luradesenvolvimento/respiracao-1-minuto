d0c5fae7cb8aebd8450be92dd218e7ae72c12d98
Rafael <raschlemper@gmail.com>
2026-01-28 13:31:10 -0300

    nova tentativa

--- src/ui/NavigationContainer.tsx
+++ src/ui/NavigationContainer.tsx
@@ -3,7 +3,7 @@ import { View } from "react-native";
 import { HomeScreen } from "../screens/HomeScreen";
 import { HistoryScreen } from "../screens/HistoryScreen";
 import { SettingsScreen } from "../screens/SettingsScreen";
-// import { PaywallScreen } from "../screens/PaywallScreen";
+import { PaywallScreen } from "../screens/PaywallScreen";
 import { TabType } from "../types";
 
 interface NavigationContainerProps {
@@ -39,13 +39,7 @@ export const NavigationContainer: React.FC<NavigationContainerProps> = ({
       case TabType.CONFIG:
         return <SettingsScreen />;
       case TabType.PREMIUM:
-        // Agora exibe a tela Paywall
-        return (
-          <HomeScreen
-            isPremium={isPremium}
-            onShowPremiumModal={onShowPremiumModal}
-            onPremiumChange={onPremiumChange}
-          />);
+        return <PaywallScreen />;
       default:
         return (
           <HomeScreen
